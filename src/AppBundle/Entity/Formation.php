074deecb6e462725510a48b2e8a4636e524de988
Unknown <belabed.jihen@gmail.com>
2018-12-24 15:29:19 +0100

    add missing entities and relations

--- src/AppBundle/Entity/Formation.php
+++ src/AppBundle/Entity/Formation.php
@@ -29,6 +29,27 @@ class Formation
     private $libelle;
 
     /**
+     * @var
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User")
+     */
+    private $assurate;
+
+
+    /**
+     * @var
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Module")
+     */
+    private $module;
+
+
+    /**
+     * @var
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Promotion")
+     */
+    private $promotion;
+
+
+    /**
      * @var string
      *
      * @ORM\Column(name="description", type="text")
@@ -92,5 +113,122 @@ class Formation
     public function getDescription()
     {
         return $this->description;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->assurate = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->module = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->promotion = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add assurate.
+     *
+     * @param \AppBundle\Entity\User $assurate
+     *
+     * @return Formation
+     */
+    public function addAssurate(\AppBundle\Entity\User $assurate)
+    {
+        $this->assurate[] = $assurate;
+
+        return $this;
+    }
+
+    /**
+     * Remove assurate.
+     *
+     * @param \AppBundle\Entity\User $assurate
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeAssurate(\AppBundle\Entity\User $assurate)
+    {
+        return $this->assurate->removeElement($assurate);
+    }
+
+    /**
+     * Get assurate.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getAssurate()
+    {
+        return $this->assurate;
+    }
+
+    /**
+     * Add module.
+     *
+     * @param \AppBundle\Entity\Module $module
+     *
+     * @return Formation
+     */
+    public function addModule(\AppBundle\Entity\Module $module)
+    {
+        $this->module[] = $module;
+
+        return $this;
+    }
+
+    /**
+     * Remove module.
+     *
+     * @param \AppBundle\Entity\Module $module
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeModule(\AppBundle\Entity\Module $module)
+    {
+        return $this->module->removeElement($module);
+    }
+
+    /**
+     * Get module.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    /**
+     * Add promotion.
+     *
+     * @param \AppBundle\Entity\Promotion $promotion
+     *
+     * @return Formation
+     */
+    public function addPromotion(\AppBundle\Entity\Promotion $promotion)
+    {
+        $this->promotion[] = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * Remove promotion.
+     *
+     * @param \AppBundle\Entity\Promotion $promotion
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removePromotion(\AppBundle\Entity\Promotion $promotion)
+    {
+        return $this->promotion->removeElement($promotion);
+    }
+
+    /**
+     * Get promotion.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
     }
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -22,6 +22,33 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=255, unique=true, nullable=true)
+     */
+    private $photo;
+
+    /**
+     * @var
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Evenement")
+     */
+    private $participate;
+
+
+    /**
+     * @var
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Formation")
+     */
+
+    private $benefit;
+
+
+    /**
+     * @var
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Promotion")
+     */
+    private $promotion;
 
     /**
      * Get id.
@@ -33,4 +60,124 @@ class User extends BaseUser
         return $this->id;
     }
 
+
+    /**
+     * Set photo.
+     *
+     * @param string|null $photo
+     *
+     * @return User
+     */
+    public function setPhoto($photo = null)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo.
+     *
+     * @return string|null
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Add participate.
+     *
+     * @param \AppBundle\Entity\Evenement $participate
+     *
+     * @return User
+     */
+    public function addParticipate(\AppBundle\Entity\Evenement $participate)
+    {
+        $this->participate[] = $participate;
+
+        return $this;
+    }
+
+    /**
+     * Remove participate.
+     *
+     * @param \AppBundle\Entity\Evenement $participate
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeParticipate(\AppBundle\Entity\Evenement $participate)
+    {
+        return $this->participate->removeElement($participate);
+    }
+
+    /**
+     * Get participate.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getParticipate()
+    {
+        return $this->participate;
+    }
+
+    /**
+     * Add benefit.
+     *
+     * @param \AppBundle\Entity\Formation $benefit
+     *
+     * @return User
+     */
+    public function addBenefit(\AppBundle\Entity\Formation $benefit)
+    {
+        $this->benefit[] = $benefit;
+
+        return $this;
+    }
+
+    /**
+     * Remove benefit.
+     *
+     * @param \AppBundle\Entity\Formation $benefit
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeBenefit(\AppBundle\Entity\Formation $benefit)
+    {
+        return $this->benefit->removeElement($benefit);
+    }
+
+    /**
+     * Get benefit.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getBenefit()
+    {
+        return $this->benefit;
+    }
+
+    /**
+     * Set promotion.
+     *
+     * @param \AppBundle\Entity\Promotion|null $promotion
+     *
+     * @return User
+     */
+    public function setPromotion(\AppBundle\Entity\Promotion $promotion = null)
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * Get promotion.
+     *
+     * @return \AppBundle\Entity\Promotion|null
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
+    }
 }
